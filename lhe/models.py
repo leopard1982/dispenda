969980d931ca378363c360278e71a4e3bdf5d3d2
leopard1982 <99190737+leopard1982@users.noreply.github.com>
2024-07-03 20:38:43 +0700

    coba

--- lhe/models.py
+++ lhe/models.py
@@ -311,3 +311,82 @@ class bab3_pkb_detail(models.Model):
         total_persen = bab3_pkb_detail.objects.all().filter(id_pkb=self.id_pkb).aggregate(Sum('selisih_persen'))
         bab3_pkb.objects.all().filter(id_pkb=self.id_pkb.id_pkb).update(selisih_angka=total_angka['selisih_angka__sum'])
         bab3_pkb.objects.all().filter(id_pkb=self.id_pkb.id_pkb).update(selisih_persen=total_persen['selisih_persen__sum'])        
+
+
+class bab3_bbnkb(models.Model):
+    id_lhe = models.ForeignKey(headerLHE,on_delete=models.RESTRICT)
+    id_bbnkb = models.CharField(max_length=36,primary_key=True,default=str(uuid.uuid4()),unique=True)
+    bulan_awal = models.PositiveSmallIntegerField(default=1,choices=BULAN)
+    bulan_akhir = models.PositiveSmallIntegerField(default=12,choices=BULAN)
+    tahun_awal = models.PositiveSmallIntegerField(default=2023)
+    tahun_akhir = models.PositiveSmallIntegerField(default=2024)
+    uppd = models.CharField(max_length=100,blank=True,null=True)
+    keterangan = models.CharField(max_length=200)
+    selisih_angka = models.BigIntegerField(default=0,blank=True,null=True)
+    selisih_persen = models.FloatField(default=0.0,blank=True,null=True)
+    createdBy = models.CharField(max_length=50,blank=False,null=False,default="")
+    createdAt = models.CharField(max_length=50,blank=False,null=False,default="")
+    is_periode = models.BooleanField(null=True,blank=True)
+    obj_data_piutang = models.IntegerField(default=0,null=True,blank=True)
+    obj_data_piutang_nominal = models.BigIntegerField(default=0,null=True,blank=True)
+    obj_pelunasan_piutang = models.IntegerField(default=0,null=True,blank=True)    
+    obj_pelunasan_piutang_persen = models.FloatField(default=0,null=True,blank=True)
+    pelunasan_piutang_rupiah = models.BigIntegerField(default=0,null=True,blank=True)
+    pelunasan_piutang_persen = models.FloatField(default=0,null=True,blank=True)
+    is_new = models.BooleanField(null=True,blank=True)
+    jml_pkb_roda4 = models.IntegerField(default=0,blank=True,null=True)
+    jml_pkb_kuning = models.IntegerField(default=0,blank=True,null=True)
+    jml_pkb_hitam = models.IntegerField(default=0,blank=True,null=True)
+    penelitian_ulang = models.IntegerField(default=0,blank=True,null=True)
+    
+
+    def save(self,*args,**kwargs):
+        if self.is_periode:
+            baru = bab3_pkb.objects.all().filter(id_lhe=self.id_lhe).count()
+            print(baru)
+            if self.is_new:
+                self.id_pkb = str(uuid.uuid4())
+            self.uppd = self.id_lhe.suratTugas.lokasi
+            self.selisih_angka=0
+            self.selisih_persen=0
+            if(self.bulan_awal<self.bulan_akhir):
+                super(bab3_pkb,self).save(*args,**kwargs)
+                bab3_pkb_detail.objects.all().filter(id_pkb=bab3_pkb.objects.get(id_pkb=self.id_pkb)).delete()
+                for i in range(int(self.bulan_awal),int(self.bulan_akhir)+1):
+                    detail = bab3_pkb_detail()
+                    detail.id_pkb=bab3_pkb.objects.get(id_pkb=self.id_pkb)
+                    detail.id_pkb_detail = str(uuid.uuid4())
+                    detail.tahun_awal=self.tahun_awal
+                    detail.tahun_akhir = self.tahun_akhir
+                    detail.nilai_awal=0
+                    detail.nilai_akhir=0
+                    detail.bulan=i
+                    detail.save()
+                
+        else:
+            super(bab3_pkb,self).save(*args,**kwargs)    
+
+class bab3_bbnkb_detail(models.Model):
+    id_pkb = models.ForeignKey(bab3_pkb,on_delete=models.RESTRICT)
+    id_pkb_detail = models.CharField(max_length=36,primary_key=True,default=str(uuid.uuid4()))
+    bulan = models.PositiveSmallIntegerField(default=1,choices=BULAN)
+    tahun_awal = models.PositiveSmallIntegerField(default=2024)
+    tahun_akhir = models.PositiveSmallIntegerField(default=2024)
+    nilai_awal = models.BigIntegerField(default=0)
+    nilai_akhir = models.BigIntegerField(default=0)
+    selisih_angka = models.BigIntegerField(default=0,blank=True,null=True)
+    selisih_persen = models.FloatField(default=0.0,blank=True,null=True)
+    createdBy = models.CharField(max_length=50,blank=False,null=False,default="")
+    createdAt = models.CharField(max_length=50,blank=False,null=False,default="")
+
+    def save(self,*args,**kwargs):
+        self.selisih_angka=self.nilai_akhir-self.nilai_awal
+        try:
+            self.selisih_persen=(self.selisih_angka)/self.nilai_awal
+        except:
+            self.selisih_persen=0
+        super(bab3_pkb_detail,self).save(*args,**kwargs)
+        total_angka = bab3_pkb_detail.objects.all().filter(id_pkb=self.id_pkb).aggregate(Sum('selisih_angka'))
+        total_persen = bab3_pkb_detail.objects.all().filter(id_pkb=self.id_pkb).aggregate(Sum('selisih_persen'))
+        bab3_pkb.objects.all().filter(id_pkb=self.id_pkb.id_pkb).update(selisih_angka=total_angka['selisih_angka__sum'])
+        bab3_pkb.objects.all().filter(id_pkb=self.id_pkb.id_pkb).update(selisih_persen=total_persen['selisih_persen__sum'])        
